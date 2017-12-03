fceb1363b90e84fdf30a63e7f0f6de35c7609e21
watchernyu <cw1681@nyu.edu>
2017-12-02 20:06:40 -0500

    now can generate 6 different settings

--- adv_gen/adv_generator.py
+++ adv_gen/adv_generator.py
@@ -17,9 +17,13 @@ valid_question_filename = "valid-v1.1-question.txt"
 valid_story_filename = "valid-v1.1-story.txt"
 valid_answer_filename = "valid-v1.1-answer-range.txt"
 
-new_data_folder_path = "../squad_adv_test"
+new_data_folder_path = "../squad_adv_2" # the data generated will be put into this folder
 
 N_ADV_WORDS = 10 # this is the number of random words that we put at the end of the sentence (or elsewhere)
+
+INSERT_MODE_FRONT = "FRONT"
+INSERT_MODE_END = "END"
+INSERT_MODE_ANYPLACE = "ANYPLACE"
 
 def get_1000_common():
     # this function will return a list,
@@ -31,16 +35,19 @@ def get_1000_common():
 
 COMMON_WORDS = get_1000_common()
 
-def lineToTokens(line):
-    # use this function to convert question to tokens, so it's easier to use
+def questionLineToTokens(line):
+    # use this function to convert a single question (in the form of a line) to tokens, so it's easier to use
     # when we try to generate new word
     tokens = line.split(" ")
     return tokens[:-1]
 
 def questionsToTokens(questions):
+    # use this function to convert a list of questions to tokens (each question in the form of a line)
+    tokenized_questions = []
+
     for i in range(len(questions)):
-        questions[i] = lineToTokens(questions[i])
-    return questions
+        tokenized_questions.append(questionLineToTokens(questions[i]))
+    return tokenized_questions
 
 # New Rule: Generate sequence of new words that contains at least some question words.
 def sample_common(common, num_sample):
@@ -49,6 +56,9 @@ def sample_common(common, num_sample):
     return sampled_common
 
 def generate_sequence(sampled_common, question, num_question_words, num_common_words):
+    # this function returns a sentence in the form of a list
+    # the sentence has at least <num_question_words> words from question line
+    # and at least <num_common_words> words from the common words
     common_num = random_num = random.randint(num_common_words, N_ADV_WORDS - num_question_words)
     common = list(np.random.choice(sampled_common, common_num))
     q = list(np.random.choice(question, N_ADV_WORDS - common_num))
@@ -56,33 +66,15 @@ def generate_sequence(sampled_common, question, num_question_words, num_common_w
     random.shuffle(sequence)
     return sequence + ['.']
 
-def generate_new_word(base, num_question_words):
-    # generate a new word randomly, this word is picked from either the 1000 common words
-    # or from the words in the question (the question is given to the function in the form of tokens)
-    
-    random_num = random.randint(0,len(base)-1) #Return a random integer x such that a <= x <= b
-    word = base[random_num]
-
-    if random_num < n_common:
-        word = common[random_num]
-    else:
-        word = question[random_num%n_common]
-
-    # print total_n
-    # print common[:5]
-    # print question
-    # print random_num
-    # print random_num%n_common
-    # print word
-    return word, num_question_words
-
 def write_to_file(file,listOfLines):
     # this function will write a list of sentences to a txt file
     for line in listOfLines:
         file.write(line+"\n")
 
-def generate_adversarial_data(original_data_folder_path,story_filename,question_filename,new_data_folder_path, answer_filename):
+def generate_adversarial_data(original_data_folder_path,story_filename,question_filename,new_data_folder_path, answer_filename,insert_mode=INSERT_MODE_ANYPLACE,num_insertions=1):
     # this function should be run for training data and validation data
+    if not os.path.exists(new_data_folder_path):
+        os.mkdir(new_data_folder_path)
 
     story_path = os.path.join(original_data_folder_path, story_filename)
     question_path = os.path.join(original_data_folder_path, question_filename)
@@ -92,7 +84,8 @@ def generate_adversarial_data(original_data_folder_path,story_filename,question_
     story_lines = train_story_file.read().strip().split("\n")
 
     train_question_file = open(question_path,"r")
-    question_lines = questionsToTokens(train_question_file.read().strip().split("\n"))
+    raw_question_lines = train_question_file.read().strip().split("\n")
+    question_lines = questionsToTokens(raw_question_lines)
 
     train_answer_file = open(answer_path, "r")
     answer_lines = train_answer_file.read().strip().split("\n")
@@ -111,55 +104,72 @@ def generate_adversarial_data(original_data_folder_path,story_filename,question_
     adv_train_answer_filepath = os.path.join(new_data_folder_path, answer_filename)
     adv_train_answer_file = open(adv_train_answer_filepath, "w")
 
+    adv_train_question_filepath = os.path.join(new_data_folder_path, question_filename)
+    adv_train_question_file = open(adv_train_question_filepath, "w")
+
     n = len(story_lines)
     new_story_lines = []
     new_answer_lines = []
+    new_question_lines = [] #we need new question lines because some questions (data entries) are corrupted for
+    # some reason. Might be a problem of SQuAD, so in the new question lines, we delete those invalid questions
     for i in range(n): # for each line
         answer_list = answer_lines[i].strip().split(':')
-        if len(question_lines[i]) > 1 and len(answer_list) == 2:
-        #try:
-            sampled_common = sample_common(COMMON_WORDS, 20)
-            sequence = generate_sequence(sampled_common, question_lines[i], 4, 3)
-            #print (sequence)
-        #for j in range(N_ADV_WORDS): # for each adversarial word
-        #    word = generate_new_word(COMMON_WORDS,question_lines[i])
-        #    added_line+=word+" "
-            story_token_list = story_lines[i].strip().split()
-            insert_indices = [i for i, x in enumerate(story_token_list) if x == "."]
-            insert_indices = [0] + insert_indices
-            insert = np.random.choice(insert_indices,1)[0]
+        story_token_list = story_lines[i].strip().split()
 
-            #print ('insert',insert)
-            # compare insert position with answer range to see if we need to change answer range
-            answer_start = int(answer_list[0])
-            answer_end = int(answer_list[1])
-            #print ('answer_start',answer_start)
+        if len(question_lines[i]) > 1 and len(answer_list) == 2: # if this line is valid
+            # if the question is valid, include it in the new question file
+            new_question_lines.append(raw_question_lines[i])
 
-            # split the story into two parts
-            if insert != 0:
-                first_half_story = story_token_list[:insert+1]
-                second_half_story = story_token_list[insert+1:]
-            else:
-                first_half_story = []
-                second_half_story = story_token_list
+            for j in range(num_insertions):
+                sampled_common = sample_common(COMMON_WORDS, 20)
+                sequence = generate_sequence(sampled_common, question_lines[i], 4, 3)
 
-            if insert <= answer_start:
-                #print ('i',i)
-                answer_start += len(sequence)
-                answer_end += len(sequence)
+                insert_indices = [i for i, x in enumerate(story_token_list) if x == "."]
+                insert_indices = [0] + insert_indices
+                insert = np.random.choice(insert_indices,1)[0]
 
-                #print ('new_answer_start', answer_start)
-            new_story_list = first_half_story + sequence + second_half_story
-            new_story_line = " ".join(new_story_list)
-            new_story_lines.append(new_story_line)
+                #print ('insert',insert)
+                # compare insert position with answer range to see if we need to change answer range
+                answer_start = int(answer_list[0])
+                answer_end = int(answer_list[1])
+                #print ('answer_start',answer_start)
 
-            new_answer_list = [str(answer_start), str(answer_end)]
-            new_answer_line = ":".join(new_answer_list)
-            new_answer_lines.append(new_answer_line)
-        else:
-            print i 
-            print question_lines[i]
-            print answer_lines[i]
+
+                if insert_mode==INSERT_MODE_FRONT:
+                    insert = 0
+                elif insert_mode == INSERT_MODE_END:
+                    insert = len(story_token_list)-1
+
+                # split the story into two parts
+                if insert == 0: #insert at the beginning
+                    first_half_story = []
+                    second_half_story = story_token_list
+                else: # insert anywhere
+                    first_half_story = story_token_list[:insert + 1]
+                    second_half_story = story_token_list[insert + 1:]
+
+                if insert <= answer_start:
+                    #print ('i',i)
+                    answer_start += len(sequence)
+                    answer_end += len(sequence)
+
+                    #print ('new_answer_start', answer_start)
+                new_story_list = first_half_story + sequence + second_half_story
+                new_answer_list = [str(answer_start), str(answer_end)]
+
+                if j == num_insertions - 1: # if this is the last insertion for this data entry
+                    new_story_line = " ".join(new_story_list)
+                    new_story_lines.append(new_story_line)
+                    new_answer_line = ":".join(new_answer_list)
+                    new_answer_lines.append(new_answer_line)
+                else:
+                    story_token_list = new_story_list
+                    answer_list = new_answer_list
+        else: # if this line is invalid, just skip it
+            pass
+            # print i
+            # print question_lines[i]
+            # print answer_lines[i]
         #except:
         #    pass
     # print story_lines[0]
@@ -167,12 +177,28 @@ def generate_adversarial_data(original_data_folder_path,story_filename,question_
     # print story_lines[2]
     write_to_file(adv_train_story_file,new_story_lines)
     write_to_file(adv_train_answer_file,new_answer_lines)
+    write_to_file(adv_train_question_file,new_question_lines)
     print "adversarial data generated at "+adv_train_story_filepath
 
-generate_adversarial_data(original_data_folder_path, train_story_filename, train_question_filename, new_data_folder_path,train_answer_filename)
-generate_adversarial_data(original_data_folder_path, valid_story_filename, valid_question_filename, new_data_folder_path,valid_answer_filename)
+# generate_adversarial_data(original_data_folder_path, train_story_filename, train_question_filename, new_data_folder_path,train_answer_filename)
+# generate_adversarial_data(original_data_folder_path, valid_story_filename, valid_question_filename, new_data_folder_path,valid_answer_filename)
 
-#
-# for line in lines:
-#
+def generate_adv_train_and_valid(original_data_folder_path,new_folder_path,insertion_mode,num_insertion):
+    # this function will generate both train and valid adv data, then
+    generate_adversarial_data(original_data_folder_path, train_story_filename, train_question_filename,
+                              new_folder_path,train_answer_filename,insertion_mode, num_insertion)
+    generate_adversarial_data(original_data_folder_path, valid_story_filename, valid_question_filename,
+                              new_folder_path, valid_answer_filename, insertion_mode, num_insertion)
+
+
+# for each setting we generate a folder containing the corresponding adv data
+settings = [["../advdata/advdata_front_only","FRONT",1],
+            ["../advdata/advdata_end_only", "END",   1],
+            ["../advdata/advdata_any_1", "ANYPLACE", 1],
+            ["../advdata/advdata_any_2", "ANYPLACE", 2],
+            ["../advdata/advdata_any_3", "ANYPLACE", 3],
+            ["../advdata/advdata_any_4", "ANYPLACE", 4]]
+
+for setting in settings:
+    generate_adv_train_and_valid(original_data_folder_path,setting[0],setting[1],setting[2])
 
