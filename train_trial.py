d7ed4ca2bcc2ebbcb3444722b7c8f3cdaa139aa2
watchernyu <cw1681@nyu.edu>
2017-12-15 16:50:24 -0500

    more debugging

--- train_trial.py
+++ train_trial.py
@@ -325,6 +325,15 @@ def the_main_function(name_of_model,config_dir='config', update_dict=None,data_f
     # here we should change the word_vocab etc so that the model knows it's going to do testing on other test sets!!!!!!
     # otherwise the model gets errors
 
+    logger.info("evaluate on add any 4 test set------------------------------------------------------------------\n")
+    test_f1, test_em, test_nll_loss = evaluate(model=_model, data=add_any_4_testdata, criterion=criterion,
+                                               trim_function=squad_trim, char_level_func=add_char_level_stuff,
+                                               word_id2word=word_vocab, char_word2id=char_word2id,
+                                               batch_size=valid_batch_size, enable_cuda=model_config['scheduling']['enable_cuda'])
+    logger.info("------------------------------------------------------------------------------------\n")
+    logger.info("nll loss=%.5f, f1=%.5f, em=%.5f" % (test_nll_loss, test_f1, test_em))
+    testplotlog_fpt.write("AddAny4\t"+str(test_nll_loss) + "\t" + str(test_f1) + "\t" + str(test_em) + "\n")
+    testplotlog_fpt.flush()
 
     logger.info("evaluate on add any 4 test set------------------------------------------------------------------\n")
     test_f1, test_em, test_nll_loss = evaluate(model=_model, data=add_any_4_testdata, criterion=criterion,
@@ -335,6 +344,12 @@ def the_main_function(name_of_model,config_dir='config', update_dict=None,data_f
     logger.info("nll loss=%.5f, f1=%.5f, em=%.5f" % (test_nll_loss, test_f1, test_em))
     testplotlog_fpt.write("AddAny4\t"+str(test_nll_loss) + "\t" + str(test_f1) + "\t" + str(test_em) + "\n")
     testplotlog_fpt.flush()
+
+
+
+
+
+
     logger.info("evaluate on add one sent test set------------------------------------------------------------------\n")
     test_f1, test_em, test_nll_loss = evaluate(model=_model, data=add_one_sent_testdata, criterion=criterion,
                                                trim_function=squad_trim, char_level_func=add_char_level_stuff,
